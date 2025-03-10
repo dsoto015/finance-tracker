114b72a7a72f29dbf2f9ba73b1ee6b103e02b99c
dsoto015 <dsoto015@yahoo.com>
2025-03-09 21:22:35 -0700

    started setup component

--- src/app/setup/setup.module.ts
+++ src/app/setup/setup.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+import { FormsModule } from '@angular/forms';
+
 import { SetupComponent } from './setup.component';
 
 
@@ -8,7 +16,12 @@ import { SetupComponent } from './setup.component';
   declarations: [SetupComponent],
   imports: [
     CommonModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatInputModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule, MatIconModule,
+    FormsModule 
   ],
   exports: [SetupComponent]
 })
