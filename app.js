fe2c4db4f0724d3bfffc3ccd8dfc6e8c7b118e36
dsoto015 <dsoto015@yahoo.com>
2025-04-14 23:12:51 -0700

    up that electron budget

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function createWindow() {
     })
   );
 
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', function () {
     mainWindow = null;
